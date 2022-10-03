e1d2d18b3b527a7b64a5f9e6f6df50196c384552
AlekseyScull <ficusoff@gmail.com>
2022-10-03 19:25:43 +0500

    multithreading lesson 4 tasks 1-6

---
+++ 3.javamultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void print() {
+
+    }
+    public String toString() {
+
+        return null;
+    }
+}

585b733a39f46f91985ffbe47f727a121306c4d0
AlekseyScull <ficusoff@gmail.com>
2022-10-04 19:17:55 +0500

    level 24, tasks 7-12

---
+++ 3.javamultithreading/src/com/javarush/task/task24/task2412/Solution.java
@@ -0,0 +1,134 @@
+package com.javarush.task.task24.task2412;
+
+import java.text.ChoiceFormat;
+import java.text.Format;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/* 
+Знания - сила!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Stock> stocks = getStocks();
+        sort(stocks);
+        Date actualDate = new Date();
+        printStocks(stocks, actualDate);
+    }
+
+    public static void printStocks(List<Stock> stocks, Date actualDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        double[] filelimits = {0d, actualDate.getTime()};
+        String[] filepart = {"change {4}", "open {2} and last {3}"};
+
+        ChoiceFormat fileform = new ChoiceFormat(filelimits, filepart);
+        Format[] testFormats = {null, null, dateFormat, fileform};
+        MessageFormat pattform = new MessageFormat("{0}   {1} | {5} {6}");
+        pattform.setFormats(testFormats);
+
+        for (Stock stock : stocks) {
+            String name = ((String) stock.get("name"));
+            String symbol = (String) stock.get("symbol");
+            double open = !stock.containsKey("open") ? 0 : ((double) stock.get("open"));
+            double last = !stock.containsKey("last") ? 0 : ((double) stock.get("last"));
+            double change = !stock.containsKey("change") ? 0 : ((double) stock.get("change"));
+            Date date = (Date) stock.get("date");
+            Object[] testArgs = {name, symbol, open, last, change, date, date.getTime()};
+            System.out.println(pattform.format(testArgs));
+        }
+    }
+
+    public static void sort(List<Stock> list) {
+        list.sort(new Comparator<Stock>() {
+            public int compare(Stock stock1, Stock stock2) {
+                int result = 0;
+
+                double open1 = !stock1.containsKey("open") ? 0 : ((double) stock1.get("open"));
+                double last1 = !stock1.containsKey("last") ? 0 : ((double) stock1.get("last"));
+                double change1 = !stock1.containsKey("change") ? last1 - open1 : ((double) stock1.get("change"));
+                double open2 = !stock2.containsKey("open") ? 0 : ((double) stock2.get("open"));
+                double last2 = !stock2.containsKey("last") ? 0 : ((double) stock2.get("last"));
+                double change2 = !stock2.containsKey("change") ? last2 - open2 : ((double) stock2.get("change"));
+/*
+Вес аргумента в сортировке 1
+                if (change1 < change2) {
+                    result += 1;
+                } else if (change1 > change2) {
+                    result -= 1;
+                }
+*/
+                long date1 = ((Date) stock1.get("date")).getTime() / (24 * 60 * 60 * 1000);
+                long date2 = ((Date) stock2.get("date")).getTime() / (24 * 60 * 60 * 1000);
+/*
+Вес аргумента 10
+                if (date1 < date2) {
+                    result += 10;
+                } else if (date1 > date2) {
+                    result -= 10;
+                }
+*/
+
+                String name1 = (String) stock1.get("name");
+                String name2 = (String) stock2.get("name");
+// тут был вес 100
+
+                //правильное решение, не приходится сравнивать аргументы всеми способами
+                return (result = name1.compareTo(name2)) != 0 ? result : (result = (int)(date2 - date1)) != 0 ? result : (int)(change2 - change1) * 1000;
+            }
+        });
+    }
+
+    public static class Stock extends HashMap<String, Object> {
+        public Stock(String name, String symbol, double open, double last) {
+            put("name", name);
+            put("symbol", symbol);
+            put("open", open);
+            put("last", last);
+            put("date", getRandomDate(2020));
+        }
+
+        public Stock(String name, String symbol, double change, Date date) {
+            put("name", name);
+            put("symbol", symbol);
+            put("change", change);
+            put("date", date);
+        }
+    }
+
+    public static List<Stock> getStocks() {
+        List<Stock> stocks = new ArrayList<>();
+
+        stocks.add(new Stock("Fake Apple Inc.", "AAPL", 125.64, 123.43));
+        stocks.add(new Stock("Fake Cisco Systems, Inc.", "CSCO", 25.84, 26.3));
+        stocks.add(new Stock("Fake Google Inc.", "GOOG", 516.2, 512.6));
+        stocks.add(new Stock("Fake Intel Corporation", "INTC", 21.36, 21.53));
+        stocks.add(new Stock("Fake Level 3 Communications, Inc.", "LVLT", 5.55, 5.54));
+        stocks.add(new Stock("Fake Microsoft Corporation", "MSFT", 29.56, 29.72));
+
+        stocks.add(new Stock("Fake Nokia Corporation (ADR)", "NOK", .1, getRandomDate()));
+        stocks.add(new Stock("Fake Oracle Corporation", "ORCL", .15, getRandomDate()));
+        stocks.add(new Stock("Fake Starbucks Corporation", "SBUX", .03, getRandomDate()));
+        stocks.add(new Stock("Fake Yahoo! Inc.", "YHOO", .32, getRandomDate()));
+        stocks.add(new Stock("Fake Applied Materials, Inc.", "AMAT", .26, getRandomDate()));
+        stocks.add(new Stock("Fake Comcast Corporation", "CMCSA", .5, getRandomDate()));
+        stocks.add(new Stock("Fake Sirius Satellite", "SIRI", -.03, getRandomDate()));
+
+        return stocks;
+    }
+
+    public static Date getRandomDate() {
+        return getRandomDate(1970);
+    }
+
+    public static Date getRandomDate(int startYear) {
+        int year = startYear + (int) (Math.random() * 30);
+        int month = (int) (Math.random() * 12);
+        int day = (int) (Math.random() * 28);
+        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+        return calendar.getTime();
+    }
+}
+

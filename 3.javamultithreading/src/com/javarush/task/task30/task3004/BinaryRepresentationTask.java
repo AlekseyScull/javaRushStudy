65d00ec2b9ed1c1c59d57b9ebe5639513b5085a9
AlekseyScull <ficusoff@gmail.com>
2022-12-16 19:38:10 +0500

    level 10, task 4,5,13, logical operations

---
+++ 3.javamultithreading/src/com/javarush/task/task30/task3004/BinaryRepresentationTask.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task30.task3004;
+
+import java.util.concurrent.RecursiveTask;
+
+public class BinaryRepresentationTask extends RecursiveTask<String> {
+    private int x;
+
+    public BinaryRepresentationTask(int x) {
+        this.x = x;
+    }
+
+    @Override
+    protected String compute() {
+        int a = x % 2;
+        int b = x / 2;
+        String result = String.valueOf(a);
+        BinaryRepresentationTask brt = new BinaryRepresentationTask(b);
+        if (b > 0) return brt.fork().join() + result;
+        return result;
+
+    }
+}

727ae8acad60397910ca2112bf78abc3754ed033
AlekseyScull <ficusoff@gmail.com>
2023-03-05 16:10:49 +0500

    level 36, task 4, Red-Black Tree refactoring

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task36.task3604;
+
+/* 
+Разбираемся в красно-черном дереве
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        RedBlackTree rbt = new RedBlackTree();
+        rbt.insert(1);
+        rbt.insert(2);
+    }
+}
